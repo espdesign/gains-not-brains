44466170bddf65b943eb1f0d41ac345d8346f0e9
espdesign <evanpendergraft@gmail.com>
2025-10-05 17:16:52 -0400

    added ability to click to add plates.

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 #app.py
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from gym_equipment import Barbell, Gym_equipment, change_barbell_weight
 
 app = Flask(__name__)
@@ -33,6 +33,25 @@ def index():
 
     return render_template('index.html', **current_state)
 
+@app.route('/add_plate/<float:plate_weight>')
+def add_plate(plate_weight):
+    barbell = current_state['barbell']
+    gym = current_state['gym']
+    if gym.plates[plate_weight] > 0:
+        barbell.add_plate(plate_weight)
+        gym.remove_plates(plate_weight, 2)
+    return redirect(url_for('index'))
+
+@app.route('/remove_plate/<float:plate_weight>')
+def remove_plate(plate_weight):
+    barbell = current_state['barbell']
+    gym = current_state['gym']
+    if barbell.cur_plate_order and barbell.cur_plate_order[-1] == plate_weight:
+        barbell.remove_plate(plate_weight)
+        gym.add_plates(plate_weight, 2)
+    return redirect(url_for('index'))
+
 if __name__ == '__main__':
     app.run(debug=True)
+
 
